3eed4397d87c79b18c526e6dc38a0bf22bd5e2fd
Shawn Namdar <ShawnNamdar@SystemsTeamMacBookPro.local>
2015-12-12 00:27:38 -0800

    more functional stuff

--- NSWindowShopper/CategorySelectorViewController.swift
+++ NSWindowShopper/CategorySelectorViewController.swift
@@ -13,21 +13,24 @@ class CategorySelectorViewController : UIViewController{
 
     var searchSettingsDTO : SearchSettingsDTO? = nil
     
-func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
     return 1
-}
+    }
 
-func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
     return searchSettingsDTO!.categoryType.count
-}
+    }
 
-func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
     return searchSettingsDTO!.categoryType[row].description
-}
+    }
 
-func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-{
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
     
-}
+    }
 
+    @IBAction func handleSelectCategoryButtonPushed(sender: UIButton) {
+    self.navigationController?.popViewControllerAnimated(true)
+    }
 }
